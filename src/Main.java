ade32ea5919be525b9288e21b752fec216cc38b7
Artur Logan <arturlogan1ferreira@gmail.com>
2024-04-28 17:26:16 -0300

    Commit do projet finalizado

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,26 @@
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        List<String> lista = List.of("Carol -f", "Bernardo -m", "Andre -m", "David -m", "Elena -f");
+        Map<String, String> mapNome = new HashMap<>();
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        for (String item : lista){
+            String[] partes = item.split("\\s-");
+            mapNome.put(partes[0], partes[1]);
         }
+
+
+        List<String> chavesComValorF = mapNome.entrySet().stream()
+                .filter(entry -> entry.getValue().equals("f"))
+                .map(Map.Entry::getKey)
+                .collect(Collectors.toList());
+
+        System.out.println("Lista Mulheres "+ chavesComValorF);
     }
 }
